b374416dae685e9470b6a0ed183f20d1b667fed7
UnRealSwordMaster <138994195+UnRealSwordMaster@users.noreply.github.com>
2023-12-06 18:54:18 +0600

    Add script

---
+++ HiWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Привет мир");
+        Debug.Log("Дебаг мир");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Пик");
+    }
+}
